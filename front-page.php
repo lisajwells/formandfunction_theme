0f8134f1dc7786f0a9b9c9bd109504fe6d062e58
Lisa Wells <lisawells999@gmail.com>
2016-03-12 13:22:44 -0500

    begin positioning home-image-ctas

--- front-page.php
+++ front-page.php
@@ -3,19 +3,34 @@
  * This file adds the Home Page to the Form and Function theme.
  */
 
+//* Remove the edit link on page
+add_filter ( 'genesis_edit_post_link' , '__return_false' );
+
 add_action( 'genesis_meta', 'author_front_page_genesis_meta' );
 /**
  * Add widget support for homepage. If no widgets active, display the default loop.
  *
  */
 
-	add_action( 'genesis_before_content_sidebar_wrap', 'faf_home_add_hero' );
-	function faf_home_add_hero() {
-		echo '<div id="home-hero">
-				<img src="'. get_home_url() .'/wp-content/uploads/2016/03/home1-Chartreuse-Traditional-Industrial-Dining-Room.jpg" alt="Chartreuse Traditional Dining Room" />
-				<div class="dark-box-group" id="hero-dark-box-group"><p>Real people<br> live here.</p><h1>Interior Design and Unique Home Furnishings for the Greater Raleigh Area</h1><a class="button">Interior Design Services</a><a class="button button-light">The Shop</a></div></div>';
-	}
+add_action( 'genesis_before_content_sidebar_wrap', 'faf_home_add_hero' );
+function faf_home_add_hero() {
+	echo '<div id="home-hero">
+			<img src="'. get_home_url() .'/wp-content/uploads/2016/03/home1-Chartreuse-Traditional-Industrial-Dining-Room.jpg" alt="Chartreuse Traditional Dining Room" />
+			<div class="dark-box-group" id="hero-dark-box-group"><p>Real people<br> live here.</p><h1>Interior Design and Unique Home Furnishings for the Greater Raleigh Area</h1><a class="button">Interior Design Services</a><a class="button button-light">The Shop</a></div></div>';
+}
 
+add_action( 'genesis_entry_footer', 'faf_home_add_cta_imgs' );
+function faf_home_add_cta_imgs() {
+	echo '<div id="home-cta-imgs">
+			<div class="home-cta-img">
+				<img src="'. get_home_url() .'/wp-content/uploads/2016/03/home2-Rustic-Industrial-Kitchen.jpg" alt="Rustic-Industrial Kitchen" />
+			<div class="dark-box-group">
+				<p>Custom Diy</p>
+				<a class="button">Learn More</a>
+			</div>
+			</div>
+		</div>';
+}
 
 
 function author_front_page_genesis_meta() {
