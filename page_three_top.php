0de5551148e6cf548639cfd89fe8bcdb2946b29c
Lisa Wells <lisawells999@gmail.com>
2016-03-17 09:48:46 -0400

    three-top images alt text come from custom fields

--- page_three_top.php
+++ page_three_top.php
@@ -8,11 +8,11 @@ add_action( 'genesis_before_content_sidebar_wrap', 'faf_add_three_top' );
 function faf_add_three_top() {
     echo '<div class="three-top-wrap">
         <div class="three-top-img-div">
-            <img src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'">
+            <img src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'" alt="'. genesis_get_custom_field( 'Image_1_alt' ) .'">
         </div>
 
         <div class="three-top-img-div">
-            <img src="'. get_home_url() .'/wp-content/uploads/2016/03/home2-Rustic-Industrial-Kitchen.jpg" alt="Rustic-Industrial Kitchen">
+            <img src="'. get_home_url() . genesis_get_custom_field( 'Image_2_url' ) .'" alt="'. genesis_get_custom_field( 'Image_2_alt' ) .'">
             <div class="dark-box-group">
                 <div class="dark-box-content-wrap">
                 <h1>The Title</h1>
@@ -21,7 +21,7 @@ function faf_add_three_top() {
         </div>
 
         <div class="three-top-img-div">
-            <img src="'. get_home_url() .'/wp-content/uploads/2016/03/home2-Rustic-Industrial-Kitchen.jpg" alt="Rustic-Industrial Kitchen">
+            <img src="'. get_home_url() . genesis_get_custom_field( 'Image_3_url' ) .'" alt="'. genesis_get_custom_field( 'Image_3_alt' ) .'">
         </div>
     </div>';
 }
