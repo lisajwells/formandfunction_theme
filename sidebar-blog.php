78a85238bd5a2080f3ac8dd64b4bb07b28c8eaf1
Lisa Wells <lisawells999@gmail.com>
2016-03-30 15:21:58 -0400

    add css for blog .entry-content and .content

--- sidebar-blog.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<div id="sidebar" class="sidebar widget-area">';
-    genesis_structural_wrap( 'sidebar' );
-    do_action( 'genesis_before_sidebar_widget_area' );
-    dynamic_sidebar('blog-sidebar');
-    do_action( 'genesis_after_sidebar_widget_area' );
-    genesis_structural_wrap( 'sidebar', 'close' );
-echo '</div>';
-?>
