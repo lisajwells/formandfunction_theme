57cc001f5d51ca3827a1c1abaedcc5f9604d8722
Lisa Wells <lisawells999@gmail.com>
2016-04-04 10:05:18 -0400

    style .archive-title like .entry-title

--- category-press.php
+++ category-press.php
@@ -5,6 +5,8 @@
  */
 //* Template Name: Press
 
+add_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+
 //* Remove standard post content output
 remove_action( 'genesis_post_content', 'genesis_do_post_content' );
 remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
