2e040df554c2c233ee5406b50e1b5fff45abda8d
Lisa Wells <lisawells999@gmail.com>
2016-04-04 11:49:42 -0400

    press: remove category from entry-footer

--- category-press.php
+++ category-press.php
@@ -13,25 +13,8 @@ remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
 /* add featured image (press logo) */
 remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-// add_action( 'genesis_entry_header', 'faf_press_do_post_image', 8 );
+add_action( 'genesis_entry_header', 'sk_display_featured_image', 8 );
 
-// function faf_press_do_post_image() {
-
-//     if ( ! is_singular() && genesis_get_option( 'content_archive_thumbnail' ) ) {
-//         $img = genesis_get_image( array(
-//             'format'  => 'html',
-//             'size'    => genesis_get_option( 'image_size' ),
-//             'context' => 'archive',
-//             'attr'    => genesis_parse_attr( 'entry-image', array ( 'alt' => get_the_title() ) ),
-//         ) );
-
-//         if ( ! empty( $img ) )
-//             printf( '<a href="%s" aria-hidden="true">%s</a>', get_permalink(), $img );
-//     }
-
-// }
-//* Add post image in Entry Content above Excerpt
-add_action( 'genesis_entry_content', 'sk_display_featured_image', 9 );
 function sk_display_featured_image() {
     $image_args = array(
         'size' => 'medium',
@@ -48,6 +31,9 @@ function sk_display_featured_image() {
 
 //* Remove standard post content output
 remove_action( 'genesis_post_content', 'genesis_do_post_content' );
+
+/* no need to show category in entry_footer */
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
 //* no sidebar for press page *//
 remove_action( 'get_header', 'child_sidebar_logic' );
