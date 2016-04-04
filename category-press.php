798ab06a2931582b8858cdda5b806f29a8908672
Lisa Wells <lisawells999@gmail.com>
2016-04-04 11:16:39 -0400

    press: remove post title

--- category-press.php
+++ category-press.php
@@ -5,11 +5,15 @@
  */
 //* Template Name: Press
 
-add_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+
+    remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
 
 //* Remove standard post content output
 remove_action( 'genesis_post_content', 'genesis_do_post_content' );
 remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+
+//* no sidebar for press page *//
 remove_action( 'get_header', 'child_sidebar_logic' );
 
 
